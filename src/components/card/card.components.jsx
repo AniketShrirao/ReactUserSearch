b7de48b61a5d6425070ff6efc8b9fe2fe133a206
saniketprdxn <anikets@prdxn.com>
2021-01-25 16:15:36 +0530

    added more details of user

--- src/components/card/card.components.jsx
+++ src/components/card/card.components.jsx
@@ -5,6 +5,8 @@ export const Card = (props)=> (
     <div className="card-container">
         <img src={`https://robohash.org/${props.user.id}?set=set5&size=180x180`} alt={`user-${props.user.id}`}/>
         <h2 >{props.user.name}</h2>
-        <p >{props.user.email}</p>
+        <p >Email: {props.user.email}</p>
+        <p >Lives In: {props.user.address.city}</p>
+        <p >Phone Number: {props.user.phone}</p>
     </div>
 )
